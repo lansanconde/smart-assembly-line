64d4be38f9f022cac27a0c89ba98a95dc0ffc338
CONDE <condelansana89@gmail.com>
2026-07-14 15:43:32 +0200

    feat(data): Chaos Day Axe 2 : throttling DynamoDB validé + rapport complet

--- terraform/environments/dev/kinesis.tf
+++ terraform/environments/dev/kinesis.tf
@@ -1,14 +1,14 @@
-resource "aws_kinesis_stream" "sensors" {
-  name             = "smart-assembly-sensors"
-  shard_count      = 1
-  retention_period = 24
-
-  stream_mode_details {
-    stream_mode = "PROVISIONED"
-  }
-
-  encryption_type = "KMS"
-  kms_key_id      = aws_kms_key.main.id
-
-  tags = { Name = "smart-assembly-sensors" }
-}
+# resource "aws_kinesis_stream" "sensors" {
+#   name             = "smart-assembly-sensors"
+#   shard_count      = 1
+#   retention_period = 24
+#
+#   stream_mode_details {
+#     stream_mode = "PROVISIONED"
+#   }
+#
+#   encryption_type = "KMS"
+#   kms_key_id      = aws_kms_key.main.id
+#
+#   tags = { Name = "smart-assembly-sensors" }
+# }
